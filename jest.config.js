cbf20bed3570a5531f351b4a050867261b149d6c
Suavek Zajac <suavek.zajac@mb02183.home>
2025-06-27 16:47:39 +0200

    setup 2

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
-  moduleNameMapping: {
+  moduleNameMapper: {
     '\\.(css|less|scss)$': 'identity-obj-proxy',
+  },
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest',
   },
 }; 
